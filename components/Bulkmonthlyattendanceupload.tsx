6e01ff9a78be94fca2139a0c93a47bc10fe86acd
mdrafee02 <mohamedrafee.safi@gmail.com>
2026-03-04 21:07:22 +0400

    issues solved

--- components/Bulkmonthlyattendanceupload.tsx
+++ components/Bulkmonthlyattendanceupload.tsx
@@ -1,3 +1,4 @@
+// Bulkmonthlyattendanceupload.tsx
 import React, { useState, useEffect } from 'react';
 import * as XLSX from 'xlsx';
 import { api, BulkMonthlyAttendanceDto } from '../services/api';
